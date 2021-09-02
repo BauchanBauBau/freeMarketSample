597801bee9ebdcfdce3932e6ea0b2a0811454f64
unkoomanko <mkw3-officialmail@yahoo.co.jp>
2021-09-02 12:12:14 +0900

    voyager導入

--- resources/views/layouts/top.blade.php
+++ resources/views/layouts/top.blade.php
@@ -45,6 +45,9 @@
                                     </a>
     
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        @if(Auth::user()->role_id == 1)
+                                            <a class="dropdown-item" href="/admin">管理画面（voyager）</a>
+                                        @endif
                                         <a class="dropdown-item" href="{{ action('userController@userPage', ['id' => Auth::id()]) }}">ユーザー情報</a>
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
