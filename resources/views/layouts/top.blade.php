3c4d111497379ea3149c7fa4278d2edf1134a85f
unkoomanko <mkw3-officialmail@yahoo.co.jp>
2021-09-08 16:11:49 +0900

    top改善

--- resources/views/layouts/top.blade.php
+++ resources/views/layouts/top.blade.php
@@ -9,7 +9,11 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
+@if(Request::routeIs('itemIndex'))
 <header class="sticky-top">
+@else
+<header>
+@endif
     <div class="container">
         <div class="row">
             <div class="col-md-12">
